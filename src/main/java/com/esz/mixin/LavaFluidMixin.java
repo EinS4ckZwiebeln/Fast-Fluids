60549c68ff546672dd13c01a33018085b2b22015
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-08-03 12:48:36 +0200

    optimize(lavafluid): make use of chunk

--- src/main/java/com/esz/mixin/LavaFluidMixin.java
+++ src/main/java/com/esz/mixin/LavaFluidMixin.java
@@ -6,36 +6,38 @@ import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.LavaFluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(LavaFluid.class)
 public abstract class LavaFluidMixin extends FlowableFluid {
 
     // Optimize if block pos is viable for burning
-    private boolean hasBurnableBlock(WorldView world, BlockPos pos) {
-        BlockState blockState = ((IWorld) world).getBlockStateIfLoadedAndInBounds(pos);
+    private boolean hasBurnableBlock(Chunk chunk, BlockPos pos) {
+        BlockState blockState = ((IWorld) chunk).getBlockStateIfLoaded(pos);
         if (blockState == null) return false;
         return blockState.isBurnable();
     }
 
     // Optimize search for burnable block neighbours
     private boolean canLightFire(WorldView world, BlockPos pos) {
+        Chunk chunk = world.getChunk(pos);
         // Cache original block pos
         int posX = pos.getX();
         int posY = pos.getY();
         int posZ = pos.getZ();
         // Generic for loop is faster
         for (int x = -1; x <= 1; x += 2) {
-            if (!this.hasBurnableBlock(world, new BlockPos(posX + x, posY, posZ))) continue;
+            if (!this.hasBurnableBlock(chunk, new BlockPos(posX + x, posY, posZ))) continue;
             return true;
         }
         for (int z = -1; z <= 1; z += 2) {
-            if (!this.hasBurnableBlock(world, new BlockPos(posX, posY, posZ + z))) continue;
+            if (!this.hasBurnableBlock(chunk, new BlockPos(posX, posY, posZ + z))) continue;
             return true;
         }
         // Check top block first, seems more likely on average
         for (int y = 1; y >= -1; y -= 2) {
-            if (!this.hasBurnableBlock(world, new BlockPos(posX, posY + y, posZ))) continue;
+            if (!this.hasBurnableBlock(chunk, new BlockPos(posX, posY + y, posZ))) continue;
             return true;
         }
         return false;
