520bb93c50a845046d4b9800f8023d8cf59ab301
EinS4ckZwiebeln <schultzfabian@gmx.de>
2023-08-03 13:41:00 +0200

    optimize(flowablefluid): switch to better optimization technique

--- src/main/java/com/esz/mixin/FlowableFluidMixin.java
+++ src/main/java/com/esz/mixin/FlowableFluidMixin.java
@@ -1,17 +1,17 @@
 package com.esz.mixin;
 
 import com.esz.FastFluids;
-import net.minecraft.block.*;
-import net.minecraft.entity.ai.pathing.PathNodeType;
+import com.esz.IWorld;
+import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.*;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -32,31 +32,27 @@ public abstract class FlowableFluidMixin extends Fluid {
         return cachedBlockState.getFluidState();
     }
 
-    // Helper function for countNeighboringSources method
-    private boolean isSourceBlock(WorldView world, BlockPos blockPos) {
-        BlockState blockState = world.getBlockState(blockPos);
-        // Retrieve fluid state from block state
-        if (blockState.getFluidState().isStill()) {
-            return true;
-        }
-        return false;
-    }
-
     // Optimize search for source block neighbours
     private int countNeighboringSources(WorldView world, BlockPos pos) {
         int i = 0;
-        // Cache original block pos
-        int posX = pos.getX();
-        int posY = pos.getY();
-        int posZ = pos.getZ();
-        // Generic for loop is faster
-        for (int x = -1; x <= 1; x += 2) {
-            if (isSourceBlock(world, new BlockPos(posX + x, posY, posZ))) i++;
-        }
-        for (int z = -1; z <= 1; z += 2) {
-            // No more than three source blocks are relevant in any scenario
-            if (i >= 3) return i;
-            if (isSourceBlock(world, new BlockPos(posX, posY, posZ + z))) i++;
+        int relX = pos.getX() & 0xF;
+        int relZ = pos.getZ() & 0xF;
+        if (relX > 0 && relX < 15 && relZ > 0 && relZ < 15) {
+            Chunk chunk = ((IWorld) world).getChunkIfLoaded(pos);
+            if (chunk == null) return i;
+            for (Direction direction : Direction.Type.HORIZONTAL) {
+                // No more than three source blocks are relevant in any scenario
+                if (i >= 3) return i;
+                BlockPos blockPos = pos.offset(direction);
+                if (chunk.getBlockState(blockPos).getFluidState().isStill()) i++;
+            }
+        } else {
+            for (Direction direction : Direction.Type.HORIZONTAL) {
+                // No more than three source blocks are relevant in any scenario
+                if (i >= 3) return i;
+                BlockPos blockPos = pos.offset(direction);
+                if (world.getBlockState(blockPos).getFluidState().isStill()) i++;
+            }
         }
         return i;
     }
